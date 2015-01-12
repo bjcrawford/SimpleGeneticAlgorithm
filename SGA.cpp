8143a7f2fa6bec5aa4b03cceeab29ab020742ff3
Brett Crawford <brettjamescrawford@gmail.com>
2015-01-11 21:24:53 -0500

    Added a very basic main for the simple genetic algorithm program

---
+++ SGA.cpp
@@ -0,0 +1,14 @@
+/**
+ * @file SGA.cpp
+ * @author Brett Crawford
+ * @brief
+ * @details
+ */
+
+#include "SGA.hpp"
+
+int main(int argc, char **argv)
+{
+
+	return 0;
+}
---
+++ SGA.hpp
@@ -0,0 +1,15 @@
+/**
+ * @file SGA.cpp
+ * @author Brett Crawford
+ * @brief
+ * @details
+ */
+
+#ifndef SGA_HPP
+#define SGA_HPP
+
+
+
+
+
+#endif
