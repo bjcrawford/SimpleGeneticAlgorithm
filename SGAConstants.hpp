2c68cccc68dec944aedc275798fd1a181c3a889f
Brett Crawford <brettjamescrawford@gmail.com>
2015-01-12 19:20:04 -0500

    Added pi, adjusted variable names

--- SGAConstants.hpp
+++ SGAConstants.hpp
@@ -9,12 +9,13 @@
 #ifndef SGACONSTANTS_HPP
 #define SGACONSTANTS_HPP
 
+const float PI = 3.141593f;
 const int NUM_OF_GENES = 12;
-const int X_UPPER_BOUND = 2;
-const int X_LOWER_BOUND = -1;
+const int X_U_BOUND = 2;
+const int X_L_BOUND = -1;
 const int POP_SIZE = 10;
 const int MAX_GEN = 10;
-const float CROSSOVER_PROB = 0.8;
-const float MUTATION_PROB = 0.01;
+const float CROSSOVER_PROB = 0.8f;
+const float MUTATION_PROB = 0.01f;
 
 #endif
