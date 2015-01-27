ac932490119338c2d81c9b0186e1e70c1ead35dd
Brett Crawford <brettjamescrawford@gmail.com>
2015-01-27 14:19:24 -0500

    Modified main to display the best individual from each generation

--- src/SGA_Main.cpp
+++ src/SGA_Main.cpp
@@ -15,24 +15,37 @@ typedef std::vector<Individual *> ind_vec;
 int main(int argc, char **argv)
 {
 	Population *pop = new Population();
+	ind_vec ind;
+	int bestInd;
+	float bestObj;
 
 	for(int i = 0; i < MAX_GEN; i++)
 	{
 		pop->selection();
 		pop->reproduce();
-	}
 
-	ind_vec ind = pop->getPopulation();
+		ind = pop->getPopulation();
+		bestInd = 0;
+		bestObj = ind[0]->getObjValue();
+		for(int j = 0; j < POP_SIZE; j++)
+		{
+			if(ind[j]->getObjValue() > bestObj)
+			{
+				bestInd = j;
+				bestObj = ind[j]->getObjValue();
+			}
+		}
 
-	for(int i = 0; i < POP_SIZE; i++)
-	{
-		std::cout << "Individual " << i << ":" << std::endl;
-		std::cout << "  Real Value: " << ind[i]->getRealValue() << std::endl;
-		std::cout << "  Objective Value: " << ind[i]->getObjValue() << std::endl;
-		std::cout << "  Relative Fitness: " << ind[i]->getRelFitness() << std::endl;
-		std::cout << "  Genotype: " << ind[i]->getGenotype() << std::endl;
+		std::cout << "Best individual of gen " << i << std::endl;
+		std::cout << "  Individual " << bestInd << std::endl;
+		std::cout << "  Real Value: " << ind[bestInd]->getRealValue() << std::endl;
+		std::cout << "  Objective Value: " << ind[bestInd]->getObjValue() << std::endl;
+		std::cout << "  Relative Fitness: " << ind[bestInd]->getRelFitness() << std::endl;
+		std::cout << "  Genotype: " << ind[bestInd]->getGenotype() << std::endl;
 		std::cout << std::endl;
 	}
+
+	
 
 	delete pop;
 
